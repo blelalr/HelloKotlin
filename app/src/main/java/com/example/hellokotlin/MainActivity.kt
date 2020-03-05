917cf0ecd76f235cdb8146f5372f4cd4d25dc348
CI-Esther.Chen <>
2020-03-05 14:34:44 +0800

    Refactor Auth Method / Update Version Code 1.0.1

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -1,21 +1,12 @@
 package com.example.hellokotlin
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.sociallogin.Auth
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        Auth.onActivityResult(requestCode, resultCode, data)
-    }
-
 }
